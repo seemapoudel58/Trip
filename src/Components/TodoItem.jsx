4fbde92e4bc02e36bab34c8fcd570940634349a3
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2024-03-23 14:48:12 +0545

    Added a new challege on todo item's color

---
+++ src/Components/TodoItem.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const TodoItem = ({ item }) => {
+  return (
+    <li className="bg-black/70 px-3 py-1 rounded-md cursor-pointer hover:scale-105 transition-all duration-100 text-white">
+      {item.todo}
+    </li>
+  );
+};
+export default TodoItem;
--- src/Components/TodosList.jsx
+++ src/Components/TodosList.jsx
@@ -1,23 +1,22 @@
 // Always try to start your component's name with uppercase - TodosList, SectionTwo, Sectionone, Footer, Header
 
 import React from "react";
-import TodoItems from "./TodoItems";
+import TodoItem from "./TodoItem";
 
 const dummyTodo = [
-  { id: 1, todo: "Learn HTML" },
-  { id: 2, todo: "Learn JavaScript" },
-  { id: 3, todo: "Learn React Node Next" },
-  { id: 4, todo: "Live your life" },
+  { id: 1, bgColor: "bg-red-400", todo: "Learn HTML" },
+  { id: 2, bgColor: "bg-green-400", todo: "Learn JavaScript" },
+  { id: 3, bgColor: "bg-yellow-400", todo: "Learn React Node Next" },
+  { id: 4, bgColor: "bg-blue-400", todo: "Live your life" },
+  { id: 4, todo: "Rock the web development" },
 ];
 
 const TodosList = () => {
   return (
     <ul className="bg-green-200 min-h-[30vh] px-8 flex justify-center items-center gap-4 flex-wrap">
       {dummyTodo.map((item) => (
-        <TodoItems key = {item.id} props= {item} />
-
+        <TodoItem key={item.id} item={item} />
       ))}
-        
     </ul>
   );
 };
