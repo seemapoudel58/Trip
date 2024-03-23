ebe37bebbaf1f10b85455116f93a66cc8bc86611
seemapoudel58 <simapoudel6@gmail.com>
2024-03-23 11:29:34 +0545

    update in section0ne

--- src/Components/section0ne.jsx
+++ src/Components/section0ne.jsx
@@ -1,18 +1,23 @@
 import React from "react";
-import { AiOutlineArrowDown } from "react-icons/ai";
 
 function SectionOne() {
   return (
     <div className=" bg-slate-400 h-[20vh] flex items-center justify-center space-x-8 ">
       <h2 className="text-xl font-semibold">What do you need for your trip?</h2>
-      <article className="border-[0.12rem] border-black rounded-lg p-[0.25rem] flex justify-between space-x-[2rem] bg-gray-200 ">
-        <div className="flex items-center ">
-          <button>1</button>
+      <div className="">
+          <select className= "bg-gray-200 border-2 border-black outline-none rounded-md py-2 px-4">
+            <option value="option1">1</option>
+            <option value="option2">2</option>
+            <option value="option3">3</option>
+            <option value="option4">4</option>
+            <option value="option5">5</option>
+            <option value="option6">6</option>
+            <option value="option7">7</option>
+            <option value="option8">8</option>
+            <option value="option9">9</option>
+            <option value="option10">10</option>
+          </select>
         </div>
-        <div className="flex items-center cursor-pointer">
-          <AiOutlineArrowDown size={20} />
-        </div>
-      </article>
         
       <article className="rounded">
         <input type="text" placeholder="Item" className="bg-gray-200 appearance-none border-2 border-black rounded w-full py-2 px-4 text-gray-700 "/>
