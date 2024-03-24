a378539b52bffa8ed724fb6e00cd1f130010b4ac
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2024-03-24 23:22:16 +0545

    Changed todo cards dimensions

--- src/Components/TodosList.jsx
+++ src/Components/TodosList.jsx
@@ -4,10 +4,10 @@ import React from "react";
 
 const TodosList = ({ todos }) => {
   return (
-    <ul className="bg-green-200 min-h-[30vh] py-20 flex flex-row px-8 flex-wrap justify-start items-center gap-4">
+    <ul className="bg-green-200 min-h-[30vh] py-20 flex flex-row px-8 flex-wrap justify-center items-center gap-4">
       {todos.map((item, index) => (
         <li
-          className="bg-black/70 text-white px-4 py-2 rounded-md hover:scale-105 cursor-pointer transition-all"
+          className="bg-black/70 max-w-lg w-full aspect-[9/1] text-white px-4 py-2 rounded-md hover:scale-105 cursor-pointer transition-all"
           key={index}
         >
           {item}
