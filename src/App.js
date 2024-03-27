f9c1d930c6825a30b430477dcc88a47304d21d87
seemapoudel58 <simapoudel6@gmail.com>
2024-03-27 14:38:08 +0545

    add complete icon and functionality

--- src/App.js
+++ src/App.js
@@ -25,29 +25,31 @@ function App() {
     setCount(count - 1);
   };
 
+  
   const [todos, setTodos] = useState([
-    "Passport (1)",
-    "Visa (if required) (1)",
-    "Travel tickets (1 set)",
-    "Accommodation details (1)",
-    "Travel itinerary (1)",
-    "Travel insurance documents (1)",
-    "Wallet with cash, credit/debit cards (1)",
-    "Mobile phone and charger (1)",
-    "Maps or navigation apps (1)",
-    "Medications and first aid kit (1)",
-    "Travel-sized toiletries (toothbrush, toothpaste, shampoo, etc.) (1 set)",
-    "Sunscreen and insect repellent (1 each)",
-    "Weather-appropriate clothing (1 set)",
-    "Comfortable walking shoes (1 pair)",
-    "Travel pillow and blanket (1 each)",
-    "Reusable water bottle (1)",
-    "Snacks and refreshments (as needed)",
-    "Travel guidebook or language translation app (1)",
-    "Camera or smartphone for capturing memories (1)",
-    "Travel documents organizer (folder or pouch) (1)",
-    "Entertainment (books, music, games) (as needed)",
-  ]);
+    { text: "Passport (1)", completed: false },
+    { text: "Visa (if required) (1)", completed: false },
+    { text: "Travel tickets (1 set)", completed: false },
+    { text: "Accommodation details (1)", completed: false },
+    { text: "Travel itinerary (1)", completed: false },
+    { text: "Travel insurance documents (1)", completed: false },
+    { text: "Wallet with cash, credit/debit cards (1)", completed: false },
+    { text: "Mobile phone and charger (1)", completed: false },
+    { text: "Maps or navigation apps (1)", completed: false },
+    { text: "Medications and first aid kit (1)", completed: false },
+    { text: "Travel-sized toiletries (toothbrush, toothpaste, shampoo, etc.) (1 set)", completed: false },
+    { text: "Sunscreen and insect repellent (1 each)", completed: false },
+    { text: "Weather-appropriate clothing (1 set)", completed: false },
+    { text: "Comfortable walking shoes (1 pair)", completed: false },
+    { text: "Travel pillow and blanket (1 each)", completed: false },
+    { text: "Reusable water bottle (1)", completed: false },
+    { text: "Snacks and refreshments (as needed)", completed: false },
+    { text: "Travel guidebook or language translation app (1)", completed: false },
+    { text: "Camera or smartphone for capturing memories (1)", completed: false },
+    { text: "Travel documents organizer (folder or pouch) (1)", completed: false },
+    { text: "Entertainment (books, music, games) (as needed)", completed: false },
+]);
+
 
   const handleAddTodo = (todo) => {
     setTodos([...todos, todo]);
@@ -58,6 +60,21 @@ function App() {
     setTodos(filteredTodos);
   };
 
+  const handleCompleted = (index) =>{
+    const newTodo = todos.map((todo , i)=>{
+      if(index === i){
+        return { ...todo, completed:!todo.completed};
+      }
+      return todo;
+    });
+    setTodos(newTodo);
+  }
+
+
+
+
+
+
   const handleClearTodos = () => {
     setTodos([]);
   };
@@ -66,6 +83,8 @@ function App() {
     todos.length > 0
       ? false
       : "There is nothing in the todo. Start adding some.";
+
+
 
   return (
     <>
@@ -88,7 +107,7 @@ function App() {
       </div>
       <Header />
       <SectionOne ToDo={handleAddTodo} />
-      <TodosList todos={todos} onDelete={handleDeleteTodo} message={message} />
+      <TodosList todos={todos} onDelete={handleDeleteTodo} onComplete = {handleCompleted} message={message} />
 
       <SectionTwo handleClearTodos={handleClearTodos} />
       <Footer />
--- src/Components/TodosList.jsx
+++ src/Components/TodosList.jsx
@@ -2,8 +2,9 @@
 
 import React from "react";
 import { MdDelete } from "react-icons/md";
+import { FaCheckCircle } from "react-icons/fa";
 
-const TodosList = ({ todos, onDelete, message }) => {
+const TodosList = ({ todos, onDelete, onComplete, message }) => {
   // if (!todos.length)
   //   return (
   //     <p className="text-red-700 py-20 text-center font-semibold text-3xl bg-green-200 min-h-[30vh] flex flex-row px-8 flex-wrap justify-center items-center gap-4">
@@ -11,6 +12,13 @@ const TodosList = ({ todos, onDelete, message }) => {
   //     </p>
   //   );
 
+  if (!todos.length) {
+    return (
+      <p>
+        The todo list is empty. Please try to add something in the todo list.
+      </p>
+    );
+  }
   return (
     <ul className="bg-green-200 min-h-[30vh] py-20 flex flex-row px-8 flex-wrap justify-center items-center gap-4">
       {todos.map((item, index) => (
@@ -18,15 +26,29 @@ const TodosList = ({ todos, onDelete, message }) => {
           className="bg-black/70 max-w-lg w-full aspect-[9/1] text-white px-4 py-2 rounded-md cursor-pointer flex items-center justify-between "
           key={index}
         >
-          {item}
           <span
-            onClick={() => {
-              onDelete(index);
-            }}
-            className=" text-2xl transform hover:scale-[1.2] transition-transform duration-500 cursor-pointer"
+            style={{ textDecoration: item.completed ? "line-through" : "none" }}
           >
-            <MdDelete />
+            {item.text}
           </span>
+          <div className="flex items-center justify-between space-x-7">
+            <span
+              onClick={() => {
+                onComplete(index);
+              }}
+              className="text-2xl transform hover:scale-[1.2] transition-transform duration-500 cursor-pointer"
+            >
+              <FaCheckCircle />
+            </span>
+            <span
+              onClick={() => {
+                onDelete(index);
+              }}
+              className=" text-3xl transform hover:scale-[1.2] transition-transform duration-500 cursor-pointer"
+            >
+              <MdDelete />
+            </span>
+          </div>
         </li>
       ))}
       {message && (
